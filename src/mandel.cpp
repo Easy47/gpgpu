e56926cad9e01c4b5b771907bd6eab1b78121ecf
Jordan Failloux <jordan.failloux@epita.fr>
2022-05-15 16:35:03 +0200

    main done

--- src/mandel.cpp
+++ src/mandel.cpp
@@ -3,8 +3,8 @@
 
 #include <png.h>
 
-//#include <CLI/CLI.hpp>
-//#include <spdlog/spdlog.h>
+#include <CLI/CLI.hpp>
+#include <spdlog/spdlog.h>
 #include "GPU/harris_gpu.hh"
 #include "CPU/harris_cpu.hh"
 #include "png/png_handler.hh"
@@ -15,26 +15,28 @@ int main(int argc, char** argv)
   (void) argc;
   (void) argv;
 
-  char filename[] = "../img/b001.png";
-  char mode[] = "CPU";
-/*
+  std::string filename = "../img/b001.png";
+  std::string mode = "CPU";
 
   CLI::App app{"harris"};
 
   app.add_option("-o", filename, "Input image");
   app.add_set("-m", mode, {"GPU", "CPU"}, "Either 'GPU' or 'CPU'");
 
-  CLI11_PARSE(app, argc, argv);*/
+  CLI11_PARSE(app, argc, argv);
 
 
-  //if (mode == "CPU") {
-  PNG_data image_data = read_png_file(filename);
-  gpu::detect_point(image_data);
-  //}
-  /*else if (mode == "GPU")
+  spdlog::info("Running {} mode on file {}", mode, filename);
+  if (mode == "CPU") {
+	PNG_data image_data = read_png_file(filename.c_str());
+	detect_point(image_data);
+  }
+  else if (mode == "GPU")
   {
-    render(reinterpret_cast<char*>(buffer.get()), width, height, stride, niter);
-  }*/
+	PNG_data image_data = read_png_file(filename.c_str());
+	gpu::detect_point(image_data);
+    //render(reinterpret_cast<char*>(buffer.get()), width, height, stride, niter);
+  }
 
   // Save
   //write_png(buffer.get(), width, height, stride, filename.c_str());
